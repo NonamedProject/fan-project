fafaf8218bf127d5ac10ba9d82d45c170fb93827
Menboong <ruby020415@naver.com>
2018-12-25 23:44:06 +0900

    mode_3

--- ver.2/mode_3/mode_3.ino
+++ ver.2/mode_3/mode_3.ino
@@ -1,11 +1,17 @@
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
 //변수
 int mode_3Stat;      
 int mode_3Menu = 1;
+int motor;
        
 int btnUpStat;            //화살표 윗방향 상태
 int btnDownStat;          //화살표 아래방향 상태
 float setTemp = 17.0;     //설정 온도
-
+float temp;
 void temppower()
 {
   if(temp<setTemp)
@@ -18,33 +24,37 @@ void temppower()
 
 void Set_temp()   //설정 온도 출력
 {
+/*
     lcd.clear();
     lcd.setCursor(0,0);
     lcd.print("Setting Temp...");
     lcd.setCursor(0,1);
     lcd.print(setTemp);
-
+*/
 }
 
 void Current_temp() //현재 온도 출력
 {
+/*
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Current Temp");
   lcd.setCursor(0,1);
   lcd.print("17.3");
+*/
 }
 
 void setup() 
 {
   Serial.begin(115200);
+  mlx.begin();
 }
 
 void loop() 
 {
   temp = mlx.readAmbientTempC();
   Serial.println(temp);
-  if()//만약에 모드버튼이 눌렸으면
+  if(0)//만약에 모드버튼이 눌렸으면
   {
     mode_3Menu *= -1;
   }
@@ -55,12 +65,12 @@ void loop()
   if(mode_3Menu == 1)   //온도 설정 모드
   {
     Set_temp();
-    if() //업 버튼이 눌리면
+    if(0) //업 버튼이 눌리면
     {
       delay(30); // delay함수를 통해 처음 버튼을 눌렀을 때 생기는 잡신호들을 무시
       setTemp++;   //설정 온도 +1
     }  
-    if() // 다운 버튼이 눌리면
+    if(0) // 다운 버튼이 눌리면
     {
        delay(30); // delay함수를 통해 처음 버튼을 눌렀을 때 생기는 잡신호들을 무시
        setTemp--;    //설정 온도 -1
